7a1fa3411d1f840fcda0b5b257fe7c6f1786b5ce
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-08-25 17:56:31 +0900

    Update.

--- src/himekuri_java2.java
+++ src/himekuri_java2.java
@@ -1,6 +1,5 @@
 /**
- *
- * @author Takayuki Kamiyama
+ * himekuri JDK8 version.
  */
 
 import java.text.SimpleDateFormat;
@@ -13,27 +12,28 @@ class Ruco extends Thread {
     final Date date = new Date();
     final SimpleDateFormat sdf_koyomi = new SimpleDateFormat("yyyy年MM月dd日HH時mm分ss秒：EEEE");
     final static String reiwa_kanji = "令和";
-    final static String reiwa_alpha = "R";
+	// Delete when "0" becomes "10". 
+    final static String reiwa_alpha = "R0";
     final Calendar cal = Calendar.getInstance();
     final String reiwa_beta = (reiwa_kanji + (cal.get(cal.YEAR)-2018) + "年" + (cal.get(cal.MONTH)+1)+ "月" + cal.get(cal.DAY_OF_MONTH)+"日");
-    // Delete when "0" becomes "10". 
-    final String reiwa_delta = (reiwa_alpha + "0" + (cal.get(cal.YEAR) - 2018) + "." + (cal.get(cal.MONTH)+1) + "." + cal.get(cal.DAY_OF_MONTH));
+	final String reiwa_delta = (reiwa_alpha + (cal.get(cal.YEAR) - 2018) + "." + (cal.get(cal.MONTH)+1) + "." + cal.get(cal.DAY_OF_MONTH));
     final int nichi = cal.get(cal.DAY_OF_YEAR);
 
     public void himekuri() {
         try {
 
+			// thread start
             final Ruco thread = new Ruco();
             thread.setDaemon(true);
             thread.start();
 
+			// himekuri function
             System.out.println(sdf_koyomi.format(cal.getTime()));
-
             System.out.println("来年の1月1日まであと : " + (365 - nichi + 1) + "日です");
-
             cal.setTime(date);
             System.out.println(reiwa_beta + " : " + reiwa_delta);
 
+			// thread join
             thread.join();
 
         } catch (Exception e) {
@@ -50,12 +50,16 @@ class Seel extends Thread {
 
     public void himekuri() {
         try {
-            final Seel thread = new Seel();
+            // thread start
+			final Seel thread = new Seel();
             thread.start();
 
+			// System out println call
             System.out.println(himekuriVersion);
 
+			// thread join
             thread.join();
+
         } catch (Exception ex) {
             System.err.println("Exceptionエラーを捕捉しました...");
             ex.printStackTrace(System.out);
@@ -64,17 +68,26 @@ class Seel extends Thread {
 }
 
 /**
- * @author takkii(Takayuki Kamiyama) Program is SuperClass.
+ * @author takkii(Takayuki Kamiyama) himekuri_java2 is SuperClass.
  */
 public class himekuri_java2 {
     public static void main(final String[] args) {
 
-        // SubClass call
-        final Ruco c = new Ruco();
-        c.himekuri();
+		try
+		{
+			// SubClass call
+			final Ruco c = new Ruco();
+			c.himekuri();
 
-        // SubClass call
-        final Seel objc = new Seel();
-        objc.himekuri();
+			// SubClass call
+			final Seel objc = new Seel();
+			objc.himekuri();
+		}
+
+		catch (Exception exe)
+		{
+			System.err.println("Exception catch  ...");
+			exe.printStackTrace(System.out);
+		}
     }
 }
