6c1047d0cd0c99dc99666276c89db3b54b98ea4a
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-10-01 16:18:08 +0900

    Update, himekuri jdk8.

--- src/himekuri_java2.java
+++ src/himekuri_java2.java
@@ -14,7 +14,7 @@ class Ruco extends Thread {
     // Delete when "0" becomes "10".
     final static String reiwa_alpha = "R0";
     final Date date = new Date();
-    final SimpleDateFormat sdf_koyomi = new SimpleDateFormat("yyyy年MM月dd日HH時mm分ss秒：EEEE");
+    final SimpleDateFormat sdf_koyomi = new SimpleDateFormat("yyyy年MM月dd日HH時mm分ss秒 ： EEEE");
     final Calendar cal = Calendar.getInstance();
     final String reiwa_beta = (reiwa_kanji + (cal.get(Calendar.YEAR) - 2018) + "年" + (cal.get(Calendar.MONTH) + 1) + "月" + cal.get(Calendar.DAY_OF_MONTH) + "日");
     final String reiwa_delta = (reiwa_alpha + (cal.get(Calendar.YEAR) - 2018) + "." + (cal.get(Calendar.MONTH) + 1) + "." + cal.get(Calendar.DAY_OF_MONTH));
@@ -53,7 +53,7 @@ class Ruco extends Thread {
 // Seel is SubClass
 class Seel extends Thread {
     String RivisionNumber = "1.0.1";
-    String himekuriVersion = "日めくり(JDK8)の数え番号 ： ";
+    String himekuriVersion = "日めくりの数え番号 ： ";
     String version = himekuriVersion + RivisionNumber;
 
     public void himekuri() {
